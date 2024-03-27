9930599930ac3170428c7a6d54d7315b684d4be7
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-03-27 23:41:07 +0530

    setup drizzle

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -2,7 +2,8 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-const connectionString = process.env.DATABASE_URL
-const client = postgres()
-export const db = drizzle(client)
+export const db = (url: string) => {
+  const client = postgres(url)
+  return drizzle(client)
+}
 export const dbSchema = schema
--- apps/server/src/route/user.ts
+++ apps/server/src/route/user.ts
@@ -1,15 +1,19 @@
 import { Hono } from 'hono'
 import { zEmail, zObject } from '@coedit/package-zschema'
 import { zValidator } from '@hono/zod-validator'
-import { env } from 'hono/adapter'
 import { db, dbSchema } from '../db'
+import { env } from 'hono/adapter'
 
 export const usersApp = new Hono()
   .get('/', async (c) => {
-    const res = await db.select().from(dbSchema.users)
-
     const { DB_URL } = env<{ DB_URL: string }>(c)
-    console.log(DB_URL)
+    try {
+      const res = await db(DB_URL).select().from(dbSchema.users)
+      console.log(res)
+    } catch (error) {
+      console.error(error)
+    }
+
     return c.text('users')
   })
   .post(
