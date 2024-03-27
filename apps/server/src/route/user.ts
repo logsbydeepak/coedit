2233581895ae322be29e712eb90ad3d5a2346a63
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-03-27 21:26:40 +0530

    create zschema package

--- apps/server/src/route/user.ts
+++ apps/server/src/route/user.ts
@@ -1,12 +1,25 @@
 import { Hono } from 'hono'
+import { zEmail } from '@coedit/package-zschema'
+import { zValidator } from '@hono/zod-validator'
 
 export const usersApp = new Hono()
   .get('/users', (c) => {
     return c.text('users')
   })
-  .post('/login', (c) => {
-    return c.text('login')
-  })
+  .post(
+    '/login',
+    zValidator(
+      'json',
+      zEmail,
+
+      (data, c) => {
+        if (!data.success) {
+          return c.json({ error: 'invalid email' })
+        }
+        return c.text('login')
+      }
+    )
+  )
   .post('/register', (c) => {
     return c.text('register')
   })
