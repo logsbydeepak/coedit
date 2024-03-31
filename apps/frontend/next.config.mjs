621d93fc758d04d183f4b320b61caf3d9c7d576e
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-03-31 20:48:49 +0530

    add env check

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { fileURLToPath } from 'node:url'
+import createJiti from 'jiti'
+const jiti = createJiti(fileURLToPath(import.meta.url))
 
-export default nextConfig
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti('./src/env')
+
+/** @type {import('next').NextConfig} */
+export default {
+  /** ... */
+}
