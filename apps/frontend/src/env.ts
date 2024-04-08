fa50fb8eb3e1b9e31a64e042e88a2fea600d03bf
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-04-08 14:20:28 +0530

    add dev script and example env

--- apps/frontend/src/env.ts
+++ apps/frontend/src/env.ts
@@ -5,11 +5,9 @@ const zRequired = z.string().min(1, { message: 'required' }).trim()
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_BASE_URL: zRequired.url(),
     NEXT_PUBLIC_API_URL: zRequired.url(),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 })
