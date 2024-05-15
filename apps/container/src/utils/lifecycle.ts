24a408fc6d3f48cd6c5becc5528992c1ea09a14d
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-05-15 14:42:42 +0530

    remove throttle

--- apps/container/src/utils/lifecycle.ts
+++ apps/container/src/utils/lifecycle.ts
@@ -24,42 +24,15 @@ export function handleStopEvent() {
 }
 
 let timeout: Timer
-function resetTimeout() {
-  clearTimeout(timeout)
-}
 
-function startTimeout() {
+export function startTimeout() {
   timeout = setTimeout(() => {
     logger.info('Server is inactive')
     emitStop()
   }, ms('5m'))
 }
 
-export function handleInactive() {
-  startTimeout()
-  event.addListener('active', () => {
-    resetTimeout()
-    startTimeout()
-  })
-}
-
-let throttledTimeout: Timer | undefined
-function throttled(func: Function, wait: number) {
-  return function (this: any, ...args: any[]) {
-    const context = this
-    if (!throttledTimeout) {
-      func.apply(context, args)
-      throttledTimeout = setTimeout(() => {
-        throttledTimeout = undefined
-      }, wait)
-    }
-  }
-}
-
-const throttleSetActive = throttled(() => {
-  event.emit('active')
-}, ms('3m'))
-
 export function setActive() {
-  throttleSetActive()
+  clearTimeout(timeout)
+  startTimeout()
 }
