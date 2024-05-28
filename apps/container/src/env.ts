b3709d4924c08999463198925bd0108fc983068a
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-05-28 21:04:13 +0530

    fox ROOT_DOMAIN env

--- apps/container/src/env.ts
+++ apps/container/src/env.ts
@@ -5,7 +5,7 @@ import { zReqString } from '@coedit/zschema'
 const schema = z.object({
   USER_API: zReqString.url(),
   CORS_ORIGIN: zReqString.url(),
-  ROOT_DOMAIN: zReqString.url(),
+  ROOT_DOMAIN: zReqString,
 })
 
 export const env = schema.parse(process.env)
