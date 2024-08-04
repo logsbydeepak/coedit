9098b47c9314f8ab85c8b5b944a571621ff27285
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-08-04 11:03:54 +0530

    update drizzle-orm dependencies

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,8 +3,8 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DB_URL!!,
+    url: process.env.DB_URL!!,
   },
 } satisfies Config
