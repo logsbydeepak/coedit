b7b123b62d015f3501d13cf6784059ac9d8ae457
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-08-11 15:49:40 +0530

    run caddy on selected path

--- apps/orchestration/coedit-proxy/coedit_proxy.go
+++ apps/orchestration/coedit-proxy/coedit_proxy.go
@@ -2,9 +2,7 @@ package coedit_proxy
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
@@ -17,17 +15,9 @@ func init() {
 	httpcaddyfile.RegisterHandlerDirective("coedit_proxy", parseCaddyfile)
 }
 
-// Middleware implements an HTTP handler that writes the
-// visitor's IP address to a file or stream.
 type Middleware struct {
-	// The file or stream to write to. Can be "stdout"
-	// or "stderr".
-	Output string `json:"output,omitempty"`
-
-	w io.Writer
 }
 
-// CaddyModule returns the Caddy module information.
 func (Middleware) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
 		ID:  "http.handlers.coedit_proxy",
@@ -35,58 +25,34 @@ func (Middleware) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
-// Provision implements caddy.Provisioner.
 func (m *Middleware) Provision(ctx caddy.Context) error {
-	switch m.Output {
-	case "stdout":
-		m.w = os.Stdout
-	case "stderr":
-		m.w = os.Stderr
-	default:
-		return fmt.Errorf("an output stream is required")
-	}
 	return nil
 }
 
-// Validate implements caddy.Validator.
-func (m *Middleware) Validate() error {
-	if m.w == nil {
-		return fmt.Errorf("no writer")
-	}
-	return nil
-}
-
-// ServeHTTP implements caddyhttp.MiddlewareHandler.
 func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-	m.w.Write([]byte(r.RemoteAddr))
-	return next.ServeHTTP(w, r)
+	fmt.Println(r.Host)
+
+	return nil
 }
 
-// UnmarshalCaddyfile implements caddyfile.Unmarshaler.
 func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
-	d.Next() // consume directive name
+	d.Next()
 
-	// require an argument
 	if !d.NextArg() {
 		return d.ArgErr()
 	}
 
-	// store the argument
-	m.Output = d.Val()
 	return nil
 }
 
-// parseCaddyfile unmarshals tokens from h into a new Middleware.
 func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
 	var m Middleware
 	err := m.UnmarshalCaddyfile(h.Dispenser)
 	return m, err
 }
 
-// Interface guards
 var (
 	_ caddy.Provisioner           = (*Middleware)(nil)
-	_ caddy.Validator             = (*Middleware)(nil)
 	_ caddyhttp.MiddlewareHandler = (*Middleware)(nil)
 	_ caddyfile.Unmarshaler       = (*Middleware)(nil)
 )
