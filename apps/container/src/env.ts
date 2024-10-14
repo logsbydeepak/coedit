3482056750d727b5f82b228234fc438f0ad7ee7b
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-10-14 14:39:03 +0530

    remove root domain env from container and update hono

--- apps/container/src/env.ts
+++ apps/container/src/env.ts
@@ -3,7 +3,6 @@ import { z, zReqString } from '@coedit/zschema'
 const schema = z.object({
   USER_API: zReqString.url(),
   CORS_ORIGIN: zReqString.url(),
-  ROOT_DOMAIN: zReqString,
 })
 
 export const env = schema.parse(process.env)
