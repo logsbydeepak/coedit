5684db19c4357a4f791213f61fef65e9a47a6021
deepak <96181928+logsbydeepak@users.noreply.github.com>
2026-01-24 19:06:45 +0530

    remove unused path from orchestration

--- apps/orchestration/src/route/project/index.ts
+++ apps/orchestration/src/route/project/index.ts
@@ -1,10 +1,5 @@
 import { h } from '#/utils/h'
 
-import { createProject } from './create'
-import { deleteProject } from './delete'
 import { startProject } from './start'
 
-export const projectRoute = h()
-  .route('/', deleteProject)
-  .route('/', createProject)
-  .route('/', startProject)
+export const projectRoute = h().route('/', startProject)
